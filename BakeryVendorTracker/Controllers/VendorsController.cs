1d6a2d821a2ca349a03614ecdadb47585b04732f
Sean Downs <spdowns@protonmail.com>
2020-07-24 16:02:42 -0700

    add Create path in /vendors/vendorId/orders to create orders within a given vendor object.

--- BakeryVendorTracker/Controllers/VendorsController.cs
+++ BakeryVendorTracker/Controllers/VendorsController.cs
@@ -24,5 +24,27 @@ namespace BakeryVendorTracker.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
     }
+    [HttpGet("/vendors.{id}")]
+    public ActionResult Show(int vendorId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(vendorId);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+    [HttpGet("/vendors/vendorId/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderDate, int orderPrice)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderDate, orderPrice);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendors", foundVendor);
+      return View("Show", model);
+    }
   }
 }

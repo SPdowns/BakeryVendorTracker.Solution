1b087ca425044f3bc8eb7b2dee584e908c443fa6
Sean Downs <spdowns@protonmail.com>
2020-07-24 15:37:24 -0700

    add return View in VendorsController for allVendors at /vendors. Adds ordercontroller using and namespace and class

---
+++ BakeryVendorTracker/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryVendorTracker.Models;
+using System.Collections.Generic;
+
+namespace BakeryVendorTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
--- BakeryVendorTracker/Controllers/VendorsController.cs
+++ BakeryVendorTracker/Controllers/VendorsController.cs
@@ -1,1 +1,16 @@
-// all logic, routes, and views meant to work with 
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BakeryVendorTracker.Models;
+
+namespace BakeryVendorTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
